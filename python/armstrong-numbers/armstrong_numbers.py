1db258ce8558d64f5cf0c0ba35662f180ed5e233
anna-katherine <56326893+anna-katherine@users.noreply.github.com>
2020-06-24 09:21:38 -0700

    Update armstrong_numbers.py

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,11 +1,14 @@
 import math
 def is_armstrong_number(number):
     sum = 0
-    number = str(number)
-    power = len(number)
-    for i in number:
+    var = str(number)
+    power = len(var)
+    for i in var:
         sum += math.pow(int(i), power)
+        print(sum)
     return number == sum
 
-num = input("Pick a number: ")
-print(is_armstrong_number(num))
+print("This is the armstrong numbers activity.")
+#num = input('Pick a number: ')
+print(is_armstrong_number(400))
+print(is_armstrong_number(371))
