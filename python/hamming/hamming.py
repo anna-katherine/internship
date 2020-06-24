35d2547fd8ca043e596d26d06cd337b0266708cd
anna-katherine <56326893+anna-katherine@users.noreply.github.com>
2020-06-23 20:30:05 -0700

    Add files via upload

---
+++ python/hamming/hamming.py
@@ -0,0 +1,9 @@
+def distance(strand_a, strand_b):
+    distance = 0
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Strands must be the same length!")
+    else:
+        for i in range(len(strand_a)):
+            if strand_a[i] != strand_b[i]:
+                distance += 1
+        return distance
